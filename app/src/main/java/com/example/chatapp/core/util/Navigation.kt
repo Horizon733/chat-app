0344c4ac44f2408ed36fdfd6cbaa926cae143b2a
Hiten <hitenchawda24@gmail.com>
2022-03-12 14:05:49 +0530

    navigation

---
+++ app/src/main/java/com/example/chatapp/core/util/Navigation.kt
@@ -0,0 +1,14 @@
+package com.example.chatapp.core.util
+
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+fun Navigation(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        graph =
+    )
+}
---
+++ app/src/main/java/com/example/chatapp/core/util/Screen.kt
@@ -0,0 +1,7 @@
+package com.example.chatapp.core.util
+
+sealed class Screen(val route: String) {
+    object MainScreen() {
+
+    }
+}
