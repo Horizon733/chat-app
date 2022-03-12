2d197073836d9c7407260f8f2f18f85a09b375b4
Hiten <hitenchawda24@gmail.com>
2022-03-12 21:45:19 +0530

    dummydata chat rename

--- app/src/main/java/com/example/chatapp/utils/DummyDatas.kt
+++ app/src/main/java/com/example/chatapp/utils/DummyDatas.kt
@@ -1,46 +1,46 @@
 package com.example.chatapp.utils
 
-import com.example.chatapp.models.Chat
+import com.example.chatapp.models.DummyChat
 
 class DummyDatas {
     companion object {
         val dummyChats = listOf(
-            Chat(
+            DummyChat(
                 "Hiten Chawda",
                 "what is up?",
                 2,
                 "https://scontent-bom1-2.cdninstagram.com/v/t51.2885-19/s150x150/271348077_4693256004054920_969442729023832713_n.jpg?_nc_ht=scontent-bom1-2.cdninstagram.com&_nc_cat=108&_nc_ohc=9H5C5WQ9BAQAX-f3evh&tn=b95HwN57VqGACHPs&edm=ABfd0MgBAAAA&ccb=7-4&oh=00_AT9hWhA089-xvOWFnpEz1R1TIdDix1MxiRRiEBY5YupMCw&oe=61F33938&_nc_sid=7bff83",
                 lastMessageTime = "12:00 AM"
             ),
-            Chat(
+            DummyChat(
                 "Dishant Gandhi",
                 "Hey there, Can you help me?",
                 2,
                 "https://scontent-bom1-2.cdninstagram.com/v/t51.2885-19/s150x150/240712763_3818394214932896_6814711276389585401_n.jpg?_nc_ht=scontent-bom1-2.cdninstagram.com&_nc_cat=108&_nc_ohc=K6j9akvmfjkAX9NF9Jx&edm=ALbqBD0BAAAA&ccb=7-4&oh=00_AT80x8eNpX5oNnl7aWxXI6oB130_Q7Jc4DL1VCieaV7bHw&oe=61F26993&_nc_sid=9a90d6",
                 lastMessageTime = "12:00 AM"
             ),
-            Chat(
+            DummyChat(
                 "Biren Nayak",
                 "Hello, How are you",
                 2,
                 "https://scontent-bom1-1.cdninstagram.com/v/t51.2885-19/s150x150/241846639_593272098440377_1623667501816188430_n.jpg?_nc_ht=scontent-bom1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=lIcyih3qAmEAX_PPsoY&edm=ABfd0MgBAAAA&ccb=7-4&oh=00_AT8azzoSxVes-DpTx7Wl2CFz9ip9hXDR53ojLAyOdy2_fw&oe=61F31FC7&_nc_sid=7bff83",
                 lastMessageTime = "12:00 AM"
             ),
-            Chat(
+            DummyChat(
                 "Aakash Chaturvedi",
                 "Okay, Good Night :)",
                 2,
                 "https://scontent-bom1-2.cdninstagram.com/v/t51.2885-19/s150x150/250247479_212994574253875_571228397832542836_n.jpg?_nc_ht=scontent-bom1-2.cdninstagram.com&_nc_cat=107&_nc_ohc=OmwFhtUyYnUAX877Jq6&edm=ALbqBD0BAAAA&ccb=7-4&oh=00_AT_PKMaGxAvNr-myoRDJT_F9VS5rIWTzny99X-dhisilbw&oe=61F2F9F0&_nc_sid=9a90d6",
                 lastMessageTime = "12:00 AM"
             ),
-            Chat(
+            DummyChat(
                 "Random User",
                 "Random message here",
                 2,
                 "https://scontent-bom1-2.cdninstagram.com/v/t51.2885-19/s150x150/258140678_440164300831567_4990825664114687212_n.jpg?_nc_ht=scontent-bom1-2.cdninstagram.com&_nc_cat=107&_nc_ohc=MnQe9nAf3YAAX8pyyBA&edm=ABfd0MgBAAAA&ccb=7-4&oh=00_AT8QA6_snz93HfUQekj25qclotVZrj5dkVE2wZrg6YDCYQ&oe=61F30F20&_nc_sid=7bff83",
                 lastMessageTime = "12:00 AM"
             ),
-            Chat(
+            DummyChat(
                 "Unknown",
                 "Typing unknown messages",
                 2,
